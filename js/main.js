e58666a3016322c952907980ab5159789a0c2a22
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-11-12 16:22:09 -0500

    end of week8 class , trying Vue

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+const myVM = ( () => {
+    let vue_vm = new Vue({
+        // link Vue to an element in our HTML
+        el: "#app",
+
+        data: {
+            message: "Hello from Vue!",
+            aontherMessage: "This is some sample text",
+
+            collection: [
+                { name: "Justin", role: "coordinator", nickname: "nitsuj"},
+                { name: "John", role: "prof", nickname: "super chill"},
+                { name: "Joe", role: "prof", nickname: "tedy bear"}
+            ]
+        },
+
+        methods: {
+            logClicked() {
+                console.log("clicked on a prof name")
+            },
+
+            clickHeader() {
+                console.log("clicked on header")
+            }
+        }
+    }).$mount("#app"); // also connects Vue to your wrapper in HTML
+  })();
